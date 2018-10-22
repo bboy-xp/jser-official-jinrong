c965ab36c6274e79cf7d8f24ecfda2135d9039ab
unknown <1169470657@qq.com>
2018-10-23 01:50:31 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="homeContent">
+    <img src="../assets/bg/bg.png" alt="" class="bg">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
+<style scoped>
+.homeContent {
+  height: 100%;
+  width: 100%;
+  background-color: #152a44;
+}
+.bg {
+  width: 100%;
+}
+</style>
+
